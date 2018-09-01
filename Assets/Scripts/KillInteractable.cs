989003f77395911d566ad6152e81e0a6a1e7b1bd
GLAVAK <gau-gau@ya.ru>
2018-09-01 22:20:28 +0700

    Refactored KillInteractable

---
+++ Assets/Scripts/KillInteractable.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class KillInteractable : MonoBehaviour
+{
+    public bool KillPlayer;
+    public bool KillEnemy;
+    public bool DestroyOnImpact = true;
+
+    public bool DieFromPlanet;
+    public bool DamagePlanet;
+
+    public GameObject ExplosionPrefab;
+
+    private bool exploded;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!other.enabled) return;
+
+        if (KillEnemy && other.gameObject.CompareTag("Enemy"))
+        {
+            Explode();
+
+            KillInteractable killInteractable = other.gameObject.GetComponent<KillInteractable>();
+            if (killInteractable != null) killInteractable.Explode();
+            else Destroy(other.gameObject);
+        }
+        else if (KillPlayer && other.gameObject.CompareTag("Player"))
+        {
+            Explode();
+
+            PlayerControls player = other.gameObject.GetComponent<PlayerControls>();
+            if (player != null) player.Explode();
+        }
+        else if (DieFromPlanet && other.gameObject.CompareTag("Planet"))
+        {
+            Explode();
+
+            if (DamagePlanet) GameData.Instance.Expirience += 1;
+        }
+    }
+
+    public void Explode()
+    {
+        if (!DestroyOnImpact || exploded) return;
+        exploded = true;
+
+        if (ExplosionPrefab != null) Instantiate(ExplosionPrefab, transform.position, Quaternion.identity);
+
+        Destroy(this.gameObject, .1f);
+    }
+}
