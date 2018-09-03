c6214e97d3499eeb5ec3b122cdc7731e03a7a1d0
GLAVAK <gau-gau@ya.ru>
2018-09-03 09:44:43 +0700

    Pause

---
+++ Assets/TimeStopper.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TimeStopper : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Time.timeScale = 0;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1;
+    }
+}
